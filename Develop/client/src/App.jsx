68f7b9197ca6fbaa0e8a76d1152f384478e8541b
Christa Lococo <montecitocandleco@gmail.com>
2023-11-12 10:13:47 -0700

    App.jsx update

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,18 +1,29 @@
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { GET_ME } from './utils/queries'; 
+import { GET_ME } from './utils/queries';
 import Navbar from './components/Navbar';
 
+// Create Apollo Client instance
+const client = new ApolloClient({
+  uri: 'your-apollo-server-uri', // replace with your actual GraphQL server URI
+  cache: new InMemoryCache(),
+});
+
 const App = () => {
-  
+ 
+  const { loading, error, data } = useQuery(GET_ME);
 
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
