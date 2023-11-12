c90c279e1c65cf69f2e646b807c449d81437d08b
Christa Lococo <montecitocandleco@gmail.com>
2023-11-12 09:50:00 -0700

    src/app.jsx

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import { useQuery } from '@apollo/client';
+import { GET_ME } from './utils/queries'; 
 import Navbar from './components/Navbar';
 
-function App() {
+const App = () => {
+  
+
   return (
     <>
       <Navbar />
