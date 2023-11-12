adfffa5f56301d7fd5bd2929e13f2e8d9ede1f5a
Christa Lococo <montecitocandleco@gmail.com>
2023-11-12 09:13:42 -0700

    auth.js done

--- Develop/client/src/utils/auth.js
+++ Develop/client/src/utils/auth.js
@@ -1,11 +1,36 @@
 // use this to decode a token and get the user's information out of it
 import decode from 'jwt-decode';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 // create a new class to instantiate for a user
 class AuthService {
   // get user data
-  getProfile() {
-    return decode(this.getToken());
+  constructor() {
+    this.client = new ApolloClient({
+      uri: '/graphql', // Change this URI to your GraphQL server endpoint
+      cache: new InMemoryCache(),
+    });
+
+    this.getToken = this.getToken.bind(this);
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this.getProfile = this.getProfile.bind(this);
+    this.loggedIn = this.loggedIn.bind(this);
+    this.isTokenExpired = this.isTokenExpired.bind(this);
+  }
+
+  async getProfile() {
+    try {
+      const response = await this.client.query({
+        query: YOUR_ME_QUERY, // Define your ME query here
+      });
+
+      return response.data.me; // Update based on your GraphQL schema
+    } catch (error) {
+      console.error('Error fetching user profile:', error);
+      return null;
+    }
   }
 
   // check if user's logged in
@@ -32,15 +57,17 @@ class AuthService {
     return localStorage.getItem('id_token');
   }
 
-  login(idToken) {
+  async login(idToken) {
     // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
+    await this.client.resetStore(); //reset the Apollo Client store to reflect login status
     window.location.assign('/');
   }
 
-  logout() {
+  async logout() {
     // Clear user token and profile data from localStorage
     localStorage.removeItem('id_token');
+    await this.client.resetStore();//reset the Apollo Client store to reflect logout status
     // this will reload the page and reset the state of the application
     window.location.assign('/');
   }
